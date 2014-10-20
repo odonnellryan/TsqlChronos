23b23faa8908000d9b9ed59f960adc0369e4e16c
odonnellryanc <odonnellryanc@gmail.com>
2014-10-20 09:25:31 -0400

    unit test changes

--- TsqlChronos.Tests/UnitTest1.cs
+++ TsqlChronos.Tests/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TsqlChronos.Util;
+using System.Collections.Generic;
 
 namespace TsqlChronos.Tests
 {
@@ -11,16 +12,40 @@ namespace TsqlChronos.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var sql = SqlParser.Parse("SELECT * FROM Whatever");
+            var SQLQuery = @"SELECT order_id
+                            FROM suppliers 
+                            WHERE a > 3 
+                            AND b = 1 
+                            OR c > 2 
+                            AND d IN ('Blade')
+                            OR e like ('%Frame%')";
+
+            var sql = SqlParser.Parse(SQLQuery);
             var fragments = SqlUtil.EnumerateAll(sql);
+
             foreach (var fragment in fragments)
             {
-                if (fragment is NamedTableReference)
+                Console.WriteLine(fragment);
+                if (fragment is QuerySpecification)
                 {
-                    var namedTable = (NamedTableReference) fragment;
-                    Console.WriteLine(namedTable.SchemaObject.BaseIdentifier.Value);
+                    var spec = (QuerySpecification)fragment;
+                    var tbl = spec.FromClause.TableReferences[0] as NamedTableReference;
+                    Console.WriteLine(spec.SelectElements.Count + " select elements, from table "
+                        + tbl.SchemaObject.Identifiers[0].Value);
+                }
+                if (fragment is SelectStatement)
+                {
+                    var select = (SelectStatement)fragment;
+                    // cast queryexpression to queryspecification
+                    var qex = select.QueryExpression as QuerySpecification;
+                    var whereClause = qex.WhereClause as WhereClause;
+                    var booleanEx = whereClause.SearchCondition as BooleanBinaryExpression;
+                    Console.WriteLine(qex.WhereClause.SearchCondition);
+                    Console.WriteLine("testing");
                 }
             }
+            
         }
+      
     }
 }
